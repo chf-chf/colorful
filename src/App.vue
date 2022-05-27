3f14b872d825901e57e098bea34fa88d0c388f60
caohf <919746894@qq.com>
2022-05-27 14:09:03 +0800

    add file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <DemoComp /> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+// import DemoComp from './components/DemoComp.vue'
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    // DemoComp
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,241 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div style="text-align:left; margin-bottom: 20px; display: flex;">
+      <img style="width:162px;height:27.5px" src="../assets/logo-colorful.png" />
+      <div style="font-size:20px;color:#929292;margin: 0 20px;font-weight: 400;">x</div>
+      <img style="width:65px;height:27.5px" src="../assets/logo.png" />
+    </div>
+    <div style="text-align:left;font-size:17px;font-weight:600;margin-bottom:10px;">上传产品</div>
+    <div class="flex">
+      <div class="left">
+        <div class="flex">
+          <label>产品名称</label>
+          <el-input class="ipt-area" v-model="inputName" placeholder="请输入内容"></el-input>
+
+        </div>
+        <div class="flex">
+          <label>产品故事</label>
+          <el-input class="ipt-area" v-model="inputDesc" type="textarea" rows="4" placeholder="请输入内容"></el-input>
+
+        </div>
+        <div class="flex">
+          <label>产品图片/video</label>
+          <!-- <input class="file-upload" type="file" @change="onChange($event)" /> -->
+          <el-upload
+            class="upload-demo"
+            action="https://ipfs.infura.io/ipfs/"
+            :on-change="uploadFile">
+            <button class="file-upload">选择文件</button>
+            <span>{{fileName ? fileName : "未选择任何文件"}}</span>
+          </el-upload>
+
+        </div>
+        <div class="flex">
+          <label>上市时间</label>
+          <el-input class="ipt-area" v-model="inputDate" placeholder="请输入内容"></el-input>
+
+        </div>
+        <div class="flex">
+          <label>其他参数</label>
+          <el-input class="ipt-area" v-model="inputOther" type="textarea" rows="4" placeholder="请输入内容"></el-input>
+
+        </div>
+        <div class="flex">
+          <label />
+          <el-button type="primary" round @click="onUploadInfo">上传产品</el-button>
+        </div>
+      </div>
+      <div class="right">
+        <div class="wrapper">
+          <div v-show="fileUrl" style="padding-left:7px">
+            <img :src="fileUrl" />
+          </div>
+          <div v-show="!fileUrl" class="no-img">暂无图片</div>
+          <div style="padding-left: 7px;">
+            <p>产品名称：{{inputName}}</p>
+            <p>产品故事：{{inputDesc}}</p>
+            <p>上市时间：{{inputDate}}</p>
+            <p>产品参数：{{inputOther}}</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <!-- ipfs demo -->
+    <!-- <div>
+      <input type="file" @change="onChange($event)" />
+    </div>
+    <div v-if="fileUrl">
+      <img :src="fileUrl" />
+    </div>
+    <div v-if="fileUrl">
+      <button @click="onUpload">upload</button>
+    </div> -->
   </div>
 </template>
 
 <script>
+import { create } from 'ipfs-http-client'
+import {ethers} from 'ethers'
+const ipfs = create('https://ipfs.infura.io:5001/api/v0')
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      fileUrl: '',
+      inputName: '',
+      inputDesc: '',
+      inputDate: '',
+      inputOther: '',
+      fileName: ''
+    }
+  },
+  methods: {
+    async onChange(e) {
+      const file = e.target.files[0]
+      this.fileName = file.name
+
+      console.log(file, 'file');
+      try {
+        const added = await ipfs.add(file)
+        console.log(added, '88')
+        const url = `https://ipfs.io/ipfs/${added.path}`
+        this.fileUrl = url
+      } catch (error) {
+        console.log('error uploading file: ', error)
+      }
+    },
+    async uploadFile(file) {
+      console.log(file, 'file');
+      this.fileName = file.name
+      try {
+        const added = await ipfs.add(file.raw)
+        console.log(added, '88')
+        const url = `https://ipfs.io/ipfs/${added.path}`
+        this.fileUrl = url
+      } catch (error) {
+        console.log('error uploading file: ', error)
+      }
+    },
+    async onUploadInfo() {
+      console.log(this.inputOther, '999')
+      let info = {
+        name: this.inputName,
+        description: this.inputDesc,
+        img: this.fileUrl,
+        properties: {
+          base: 'starfish',
+          rich_property: {
+            name: 'type',
+            value: '3080',
+            display_value: 'High'
+          }
+        },
+        attributes: this.inputOther
+        // attributes: [
+        //   {
+        //     trait_type: 'model',
+        //     value: 'RTX3080',
+        //   },
+        //   {
+        //     display_type: 'boost_number',
+        //     trait_type: 'Power',
+        //     value: 40
+        //   },
+        //   {
+        //     display_type: 'number',
+        //     trait_type: 'Gerneration',
+        //     value: 2
+        //   }
+        // ]
+      }
+      let result = await ipfs.add(JSON.stringify(info))
+      console.log('upload', `https://ipfs.io/ipfs/${result.path}`)
+    }
+  },
+  mounted() {
+    console.log(ethers, 'ethers')
+    let privateKey = "0x0123456789012345678901234567890123456789012345678901234567890123";
+    let wallet = new ethers.Wallet(privateKey);
+    console.log(wallet.address, 'wallet.address');
+    console.log(wallet.privateKey, 'wallet.privateKey');
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+input {
+  border: none;
+  color: rgba(255, 255, 255, 0.6);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.hello {
+  padding: 30px 20%;
+  color: #fff;
 }
-li {
+.flex {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+label {
+  width: 150px;
+  text-align: left;
+  font-size: 10px;
+}
+.wrapper {
+  padding: 18px 14px 13px 12.5px;
+  background: #1F2024;
+  box-shadow: inset 4px 4px 20px #000000, inset -4px -4px 20px rgba(99, 99, 99, 0.7);
+  border-radius: 20px;
+}
+.wrapper p {
+  font-size: 10px;
+  margin-bottom: 17px;
+  text-align: left;
+}
+.wrapper .no-img {
+  width: 230px;
+  height: 120px;
+  color: 10px;
+}
+img {
+    display: inline-block;
+    width: 230px;
+    height: 120px;
+}
+.ipt-area >>> .el-input__inner, .ipt-area >>> .el-textarea__inner {
+  background: #1F2024;
+  box-shadow: -4px -4px 20px rgba(99, 99, 99, 0.31), 4px 4px 20px #000000;
+  border-radius: 5px;
+  color: rgba(255, 255, 255, 0.6);
+  border: none;
+  font-size: 10px;
+}
+.el-button--primary {
+  width: 100%;
+  background: #E4002B;
+  box-shadow: 2px 2px 10px #070002;
+  border-radius: 30px;
+  border: none;
+  font-size: 10px;
+}
+.file-upload {
+    height: 25px;
+    width: 91px;
+    background: rgb(228, 0, 43);
+    box-shadow: rgb(7 0 2 / 85%) 4px 4px 20px;
+    border-radius: 30px;
+    border: none;
+    color: #fff;
+}
+.upload-demo span {
+  font-size: 10px;
+  color: rgba(255, 255, 255, 0.6);
+  margin-left: 8px;
   display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
