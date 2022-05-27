61fa41bf4fb67dfc0b30ca185e36b7e268cb4d5c
caohf <919746894@qq.com>
2022-05-27 17:50:23 +0800

    连接钱包

--- src/components/ColorfulComp.vue
+++ src/components/ColorfulComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="colorful">
     <div style="text-align:left; margin-bottom: 20px; display: flex;">
       <img style="width: 400px;height: 100%" src="../assets/logo-colorful.png" />
     </div>
@@ -75,7 +75,13 @@
 <script>
 import { create } from 'ipfs-http-client'
 import {ethers} from 'ethers'
+import abi from '../../abi.json'
+
 const ipfs = create('https://ipfs.infura.io:5001/api/v0')
+
+// 合约地址
+const contractAddress = "0x0a21139d67449d45a1565d672C51DB52DDAC42E7";
+
 export default {
   name: 'ColorfulComp',
   props: {
@@ -83,6 +89,8 @@ export default {
   },
   data() {
     return {
+      web3: null,
+      chainId: null,
       fileUrl: '',
       inputName: '',
       inputDesc: '',
@@ -92,6 +100,48 @@ export default {
     }
   },
   methods: {
+    async connect() {
+      //此函数是链接用户钱包功能,用来将ethers实例化
+      let self = this;
+      let web3Provider, web3;
+      if (window.ethereum) {
+        web3Provider = window.ethereum;
+        try {
+          // 请求用户授权
+          await window.ethereum.enable();
+        } catch (error) {
+          // 此处用户不给授权的处理逻辑
+        }
+      } else if (window.web3) {
+        //windows.web3是用来适配旧版metamask
+        web3Provider = window.web3.currentProvider;
+      } else {
+        // 处理用户没有metamask的逻辑
+      }
+      web3 = new ethers.providers.Web3Provider(web3Provider);
+      let user = web3.getSigner();
+      //ether.js获取chainId等链信息比web3.js简洁且有逻辑的多,直接通过getNetWork即可获得链内容
+      let networkInfo = await web3.getNetwork();
+      // ethers.js和web3.js不同的点,在于可以直接获得当前钱包地址,通过上面的Signer(user)
+      let wallet_address = await user.getAddress();
+      console.log(wallet_address, 'wallet_address')
+      console.log(web3, 'web3')
+      let chainId = networkInfo.chainId;
+
+      self.web3 = web3;
+      self.chainId = chainId;
+      self.address = wallet_address;
+      window.ethereum.on("chainChanged", (chainId) => {
+        console.log("用户切换了链", chainId);
+      });
+      // 连接合约
+      let c = await new ethers.Contract(contractAddress, abi, self.web3);
+      let signer = self.web3.getSigner();
+      console.log(signer, 'signer')
+
+      // 创建连接到签名器signer
+      self.contract = c.connect(signer);
+    },
     async onChange(e) {
       const file = e.target.files[0]
       this.fileName = file.name
@@ -107,6 +157,7 @@ export default {
       }
     },
     async uploadFile(file) {
+      // 上传文件
       console.log(file, 'file');
       this.fileName = file.name
       try {
@@ -119,7 +170,8 @@ export default {
       }
     },
     async onUploadInfo() {
-      console.log(this.inputOther, '999')
+      let self = this
+      console.log('contract', self.contract)
       let info = {
         name: this.inputName,
         description: this.inputDesc,
@@ -151,15 +203,25 @@ export default {
         // ]
       }
       let result = await ipfs.add(JSON.stringify(info))
-      console.log('upload', `https://ipfs.infura.io/ipfs/${result.path}`)
+      let url = `https://ipfs.infura.io/ipfs/${result.path}`
+      console.log('upload', result)
+
+      if (self.chainId == 4) {
+        let r = await self.contract.mint([url]);
+        let tx = await r.wait();
+        console.log(tx);
+      } else {
+        alert("bad chain");
+      }
     }
   },
   mounted() {
+    // 进入页面连接钱包
+    this.connect(); 
     console.log(ethers, 'ethers')
     let privateKey = "0x0123456789012345678901234567890123456789012345678901234567890123";
     let wallet = new ethers.Wallet(privateKey);
-    console.log(wallet.address, 'wallet.address');
-    console.log(wallet.privateKey, 'wallet.privateKey');
+    console.log(wallet, 'wallet')
   }
 }
 </script>
@@ -170,7 +232,7 @@ input {
   border: none;
   color: rgba(255, 255, 255, 0.6);
 }
-.hello {
+.colorful {
   padding: 30px 20%;
   color: #fff;
 }
@@ -205,10 +267,10 @@ label {
   color: 10px;
 }
 img {
-    display: inline-block;
-    width: 230px;
-    height: 120px;
-    border-radius: 5px;
+  display: inline-block;
+  width: 230px;
+  height: 120px;
+  border-radius: 5px;
 }
 .ipt-area >>> .el-input__inner, .ipt-area >>> .el-textarea__inner {
   background: #1F2024;
@@ -227,13 +289,13 @@ img {
   font-size: 10px;
 }
 .file-upload {
-    height: 25px;
-    width: 91px;
-    background: rgb(228, 0, 43);
-    box-shadow: rgb(7 0 2 / 85%) 4px 4px 20px;
-    border-radius: 30px;
-    border: none;
-    color: #fff;
+  height: 25px;
+  width: 91px;
+  background: rgb(228, 0, 43);
+  box-shadow: rgb(7 0 2 / 85%) 4px 4px 20px;
+  border-radius: 30px;
+  border: none;
+  color: #fff;
 }
 .upload-demo span {
   font-size: 10px;
